52b2aa489f797111aba9fe14e170f027e367c5f1
MoBoustta <moboustta6@gmail.com>
2020-11-11 13:09:13 +0100

    implement the app controller

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    var choice = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI(0)
     }
 
-
+    @IBAction func choiceMadeButton(_ sender: UIButton) {
+        if sender.currentTitle == storyBrain.getChoice1() {
+            choice = storyBrain.choice1Destination()
+        }
+        else {
+            choice = storyBrain.choice2Destination()
+        }
+        updateUI(choice)
+    }
+    
+    func updateUI(_ choice: Int) {
+        storyBrain.updateStory(choice)
+        
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
 }
 
