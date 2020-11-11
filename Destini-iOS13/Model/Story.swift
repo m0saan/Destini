7cd892857605b5a51c7f91475d413fb1cd28e76d
MoBoustta <moboustta6@gmail.com>
2020-11-11 13:09:46 +0100

    implement the view

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct Story {
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String , choice2Destination: Int){
+        self.title = title
+        self.choice1 = choice1
+        self.choice1Destination = choice1Destination
+        self.choice2 = choice2
+        self.choice2Destination = choice2Destination
+    }
+    
+    var title: String
+    var choice1: String
+    var choice1Destination: Int
+    var choice2: String
+    var choice2Destination: Int
+}
